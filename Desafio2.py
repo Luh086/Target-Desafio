a31809f7804e89464fd859205dfb8cdedb4ac571
Luh086 <luholiveira086@gmail.com>
2024-09-26 22:54:17 -0300

    Update Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,19 +1,17 @@
 #DESAFIO 2 - Sequência de Fibonacci
 
-def sequencia_fibonacci(numero):
-    a, b = 0, 1
-    
-    while b < numero:
-        a, b = b, a + b
-    
-    if b == numero:
-        return True
+def is_fibonacci_number(num):
+
+    fib_sequence = [0, 1]
+    while fib_sequence[-1] < num:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+    if num in fib_sequence:
+        return f"O número {num} pertence à sequência de Fibonacci."
     else:
-        return False
+        return f"O número {num} NÃO pertence à sequência de Fibonacci."
 
-numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci:"))
+user_number = int(input("Informe um número para verificar se pertence à sequência de Fibonacci: "))
 
-if sequencia_fibonacci(numero):
-    print(f"O número {numero} pertence à sequência de Fibonacci.")
-else:
-    print(f"O número {numero} não pertence à sequência de Fibonacci.")
+result = is_fibonacci_number(user_number)
+print(result)
